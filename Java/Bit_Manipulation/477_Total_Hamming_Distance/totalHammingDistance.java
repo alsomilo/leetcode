6cc2075808b3b9c6608f10ada5f1333e57ab14c4
alsomilo <yuan.wdc@gmail.com>
2018-03-13 22:30:34 -0700

    leetcode 477 - modify

--- Java/Bit_Manipulation/477_Total_Hamming_Distance/totalHammingDistance.java
+++ Java/Bit_Manipulation/477_Total_Hamming_Distance/totalHammingDistance.java
@@ -2,22 +2,18 @@ class totalHammingDistance
 {
     public static int totalHammingDistance_new(int[] nums) 
     {
-        int ret_val = 0, mask = 0, set_count = 0;
+        int ret_val = 0, set_count = 0;
         
         for(int i=0; i<32; i++)
-        {
-            mask = (1 << i);
+        {           
             set_count = 0;
             
             for(int val : nums)
             {                
-                if((val & mask) != 0)
-                {
-                    set_count++;
-                }
+                set_count += ((val >>> i) & 1);
             }
             
-            ret_val += set_count * (nums.length - set_count);
+            ret_val += (set_count * (nums.length - set_count));
         }                
         
         return ret_val;
