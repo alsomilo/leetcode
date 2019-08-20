957e720565ce0f2de267a9e56012f9558e27296d
alsomilo <yuan.wdc@gmail.com>
2019-08-20 00:34:19 -0700

    add comments

--- Java/Bit_Manipulation/137_Single_Number_II/Sol1_naive/singleNumber.java
+++ Java/Bit_Manipulation/137_Single_Number_II/Sol1_naive/singleNumber.java
@@ -1,3 +1,26 @@
+/*
+LC-137
+
+Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: [0,1,0,1,0,1,99]
+Output: 99
+*/
+
+
+//082019: this solution used harsh set, to seperate unique number (shown at least once) and dup number (shown more than once), take the delta of (unique and dup), 
+//the delta will be the number only shown once
+
 import java.util.HashSet;
 
 class singleNumber 
--- Java/Bit_Manipulation/137_Single_Number_II/Sol2_Bit/singleNumber.java
+++ Java/Bit_Manipulation/137_Single_Number_II/Sol2_Bit/singleNumber.java
@@ -1,3 +1,28 @@
+/*
+LC-137
+
+Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: [0,1,0,1,0,1,99]
+Output: 99
+*/
+
+
+//082019: this is a genric solution applies to any accurance of dup numbers. The trick here is to use sum_bit_i % N to cancel the duplicated numbers, N is the repeated times for dup numbers.
+//  sum_bit_i % N  == 1 means that unique value has bit i set.
+
+
+
 class singleNumber {
     public static int singleNumber_now(int[] nums) 
 	{	
