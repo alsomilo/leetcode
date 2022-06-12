00aaa0750ddd8fc207382992ff25f02022e1e0d2
yuanw <35405452+alsomilo@users.noreply.github.com>
2022-06-12 00:10:36 -0700

    Time: 88 ms (56.83%), Space: 14.1 MB (75.85%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        
+        '''
         res = []
         nums.sort()
         
@@ -17,6 +17,28 @@ class Solution:
                 
                 if prevNum != nums[i]:    #cut branch logic: avoid processing branch for the node which has same value with prevNum  
                     prevNum = nums[i]
+                    dfs( nums[:i]+nums[i+1:] , path + [nums[i]], res)
+                
+                
+        dfs(nums, [], res)
+        
+        return res
+        '''
+        
+        res = []
+        nums.sort()
+        
+        def dfs(nums, path, res):
+            
+            if not nums:
+                res.append(path)
+                return 
+            
+
+            for i in range(len(nums)):
+                
+                if not (i > 0 and nums[i-1] == nums[i]):    #cut branch logic: avoid processing branch for the node which has same value with prevNum  
+                    #prevNum = nums[i]
                     dfs( nums[:i]+nums[i+1:] , path + [nums[i]], res)
                 
                 
