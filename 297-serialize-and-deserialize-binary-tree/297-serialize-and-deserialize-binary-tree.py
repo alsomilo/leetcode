4f1bae0568986d6e4667ec6217524632870fc891
yuanw <35405452+alsomilo@users.noreply.github.com>
2022-06-17 00:32:52 -0700

    Time: 228 ms (73.94%), Space: 20.4 MB (27.51%) - LeetHub

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,67 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
+class Codec:
+    
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        
+        
+        if not root:
+            return '#,'
+        
+        mid   = str(root.val)+','
+        left  = self.serialize(root.left)
+        right = self.serialize(root.right)
+        #print(mid+left+right)
+        return mid+left+right
+        
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        
+        
+        if data == '#,':
+            #print(f'empty str!')
+            return None
+        
+        
+        queue = deque(data.split(','))
+        #print(queue)
+        
+        def dfs(queue):
+            if not queue:
+                return None
+            
+            if queue[0] == '#':
+                queue.popleft()
+                #print(queue)
+                return None
+            
+            root = TreeNode(int(queue[0]))
+            queue.popleft()
+            #print(queue)
+            root.left = dfs(queue)
+            root.right = dfs(queue)
+            return root
+        
+        return dfs(queue)
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
