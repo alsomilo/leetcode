b6c2eb3dda3327b8cb3ab3d2aa297f6db669cfd4
yuanw <35405452+alsomilo@users.noreply.github.com>
2022-07-06 01:52:56 -0700

    Time: 252 ms (80.00%), Space: 25.5 MB (27.77%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -4,6 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
+        '''
         if k % len(nums) == 0:
             return
         
@@ -12,5 +13,37 @@ class Solution:
         numsC = nums[-k:] + nums[:-k]
         for i in range(len(nums)):
             nums[i] = numsC[i]
+            
+        '''
+        if k % len(nums) == 0:
+            return
+        
+        k %= len(nums)
+
+        count = 0
+        start = 0
+        
+        
+        while count < len(nums):
+            
+            i = start
+            prev = nums[start]
+        
+            while True:
+                newIdx = (i+k) % len(nums)
+
+                temp = nums[newIdx]
+                nums[newIdx] = prev
+                prev = temp
+                i = newIdx
+                count += 1
+
+                if i == start:
+                    break
+                
+            start += 1
+        
+        
+                
         
         
