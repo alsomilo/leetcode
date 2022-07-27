2b17bf1cc6a2275de608b099526df1bfa5787bab
yuanw <35405452+alsomilo@users.noreply.github.com>
2022-07-26 23:11:16 -0700

    Time: 5970 ms (20.97%), Space: 14.2 MB (8.54%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,77 +1,44 @@
 class Solution:
-    
-    '''
-    def twoSum(self, nums, start, target):
-        lo,hi = start, len(nums)-1
-        
-        delta = float('Inf')
-        while lo < hi:
-            twoSum = nums[lo] + nums[hi]
-            
-            if abs(delta) > abs(target-twoSum):
-                delta = target-twoSum
-                
-            if twoSum < target:
-                lo += 1
-            else:
-                hi -= 1
-        
-        return target-delta
-    
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         
-        if len(nums) < 3:
-            return 0
-
-        nums.sort()
-        delta = float('Inf')
-        i = 0
-        while i < len(nums)-2:
-            
-            threeSum = nums[i] + self.twoSum(nums, i+1, target-nums[i])
-            
-            if abs(delta) > abs(target- threeSum):
-                delta = target - threeSum
-            i+=1
-            
-        return target - delta
-        '''
-
-
-            
-
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
-        minAbs = float('Inf')
-        i = 0
-        nums.sort()
+        
         
         def twoSum(left, twoSumtarget):
-
+            
             lo,hi = left,len(nums)-1
             minRes = float('Inf')
-
+            
             while lo < hi:
                 left, right = nums[lo], nums[hi]
                 currSum = left + right
-
-                if abs(twoSumtarget - currSum) < abs(minRes):
-                    minRes = twoSumtarget - currSum
-
+                
+                if abs(currSum - twoSumtarget) < abs(minRes):
+                    minRes = currSum - twoSumtarget
+                    
                 if currSum < twoSumtarget:
                     lo += 1
                 else:
                     hi -= 1
-
+                        
             return twoSumtarget - minRes
-    
+                
 
+            
+        minAbs = float('Inf')
+        i = 0
+        res = 0
+        nums.sort()
+        
         while i < len(nums)-2:
             threeSum = nums[i] + twoSum(i+1, target - nums[i])
-
-            if abs(target - threeSum) < abs(minAbs):
-                minAbs = target - threeSum
-            i+=1
-
-        return target-minAbs
+            
+            if abs(threeSum - target) < abs(minAbs):
+                minAbs = threeSum - target
                 
-        
+            while i+1 < len(nums) and nums[i+1] == nums[i]:
+                i+=1
+            i+=1
+            
+        return target-minAbs
+                    
+                
