b7045c57b8f6adb041082a72c8d68787e30e6047
yuanw <35405452+alsomilo@users.noreply.github.com>
2022-08-03 00:22:59 -0700

    Time: 112 ms (61.77%), Space: 17.5 MB (50.23%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -0,0 +1,64 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        '''
+        def overlap(rangeA, rangeB):
+            if rangeA[1] > rangeB[0] and rangeA[0] < rangeB[1]:
+                #print(f'{rangeA} and {rangeB} overlap!')
+                return True
+            #print(f'{rangeA} and {rangeB} NO overlap!')
+            return False
+        
+        
+        timeline = []
+        
+        for interval in intervals:
+            timeline += interval
+            
+        timeline.sort()
+        
+        i = 0
+        maxRoom = 1
+        
+        while i+1 < len(timeline):
+            currRoom = 0
+            
+            for interval in intervals:
+                if overlap(interval, [timeline[i],timeline[i+1]]):
+                    currRoom += 1
+            
+            maxRoom = max(maxRoom, currRoom)
+            
+            i+=1
+            
+        return maxRoom
+        '''
+        
+        
+        start = [interval[0] for interval in intervals]
+        end =   [interval[1] for interval in intervals]
+        start.sort()
+        end.sort()
+        maxRoom = 0 
+        curRoom = 0
+        
+        i,j = 0,0
+        
+        while i < len(start) or j < len(end):
+            st = start[i] if i < len(start) else float('Inf')
+            et = end[j] if j < len(end) else float('Inf')
+            
+            if st < et:
+                curRoom += 1
+                maxRoom = max(maxRoom, curRoom)
+                i += 1
+            else:
+                curRoom -= 1
+                maxRoom = max(maxRoom, curRoom)
+                j += 1
+                
+        return maxRoom
+                
+            
+            
+            
