07d69037be9eaa70585ec3f2628e27e165230d30
yuanw <35405452+alsomilo@users.noreply.github.com>
2022-09-15 21:03:29 -0700

    Time: 114 ms (67.88%), Space: 13.9 MB (91.95%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,33 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        '''
+        res = []
+        
+        def dfs(start, path, combSum):
+            
+            if combSum == target:
+                res.append(path)
+            if combSum > target:
+                return
+            
+            for i in range(start, len(candidates)):
+                dfs(start, path+[candidates[i]], combSum + candidates[i])
+                
+        dfs(0, [], 0)
+        return res
+        '''
+        
+        res = []
+        
+        def dfs(candidates, path, combSum):
+            
+            if combSum == target:
+                res.append(path)
+            if combSum > target:
+                return
+            
+            for i in range(len(candidates)):
+                dfs(candidates[i:], path+[candidates[i]], combSum + candidates[i])
+                
+        dfs(candidates, [], 0)
+        return res
