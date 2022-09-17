042d94ff45c9cf67cfa05d9e9da35139e5fbc0e9
yuanw <35405452+alsomilo@users.noreply.github.com>
2022-09-17 10:39:04 -0700

    Time: 1887 ms (9.99%), Space: 71.9 MB (53.69%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -1,23 +1,31 @@
 class TimeMap:
 
     def __init__(self):
-        self.map = {}
+        #self.map = {}
+        self.map = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
+        '''
         if key not in self.map:
             self.map[key] = [[timestamp, value]]
         else:
             self.map[key].append([timestamp, value])
-
+        '''
+        self.map[key].append([timestamp, value])
+        
     def get(self, key: str, timestamp: int) -> str:
         
         if key not in self.map:
             return ""
         
         lut = self.map[key]
+        
+        #same key only have one value (only one timestamp), return this value immediatly 
         if len(lut) == 1:
             return lut[0][1]
         
+        
+        #same key have multiple value set at different previous timestamps, now find the cloest timestamp's value using bin search
         lo,hi = 0,len(lut)-1
         
         while lo <= hi:
