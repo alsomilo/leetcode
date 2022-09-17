830779e8c34a4adb8d5d3cd0f27e59ea35e31346
yuanw <35405452+alsomilo@users.noreply.github.com>
2022-09-17 02:24:33 -0700

    Time: 2034 ms (5.03%), Space: 24.8 MB (5.38%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -1,7 +1,7 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         
-        '''
+        ''' Do not use, time limit exceed!!!
         #tank = 0
         length = len(gas)
         
@@ -24,6 +24,13 @@ class Solution:
         '''
         
         #'''
+        
+        #first, calculate the net gain of gas at each station.
+        #since gas in the tank can be carried over to the next station, so this trigger the thought of prefixSum!! which also aggregate previous element's sum
+        
+        
+        #if starting from the lowest point in prefixSum and travel entire array, this make a new prefixSum array based on the new net[], if the new prefixSum array's lowest point is > 0, thenwe can travel around
+        
         net = [gas[i] - cost[i] for i in range(len(gas))]
         print(net)
         preSum = [0]
