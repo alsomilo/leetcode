4db1f19843e9b6b21430429c810657780a83a990
yuanw <35405452+alsomilo@users.noreply.github.com>
2022-09-23 21:23:56 -0700

    Time: 1747 ms (25.44%), Space: 20.4 MB (61.73%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,7 +1,7 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
-        '''
+        #'''
         def getDistance(point):
             x,y = point[0],point[1]    
             return point, x**2 +y**2
@@ -9,10 +9,10 @@ class Solution:
         dis = [getDistance(point) for point in points]
         dis.sort(key = lambda x: x[1])
         return [x[0] for x in dis][:k]
+        #'''
         '''
-        
         dis = [(point, point[0]**2 +point[1]**2) for point in points]
         dis.sort(key = lambda x: x[1])
         return [x[0] for x in dis][:k]
-        
+        '''
         
