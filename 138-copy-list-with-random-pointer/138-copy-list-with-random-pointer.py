a5c4dc00cab82e1b071956766b919080b8965c06
yuanw <35405452+alsomilo@users.noreply.github.com>
2022-09-24 14:31:43 -0700

    Time: 75 ms (19.09%), Space: 14.9 MB (83.01%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -15,16 +15,29 @@ class Solution:
         curr, currNew = dummy, newDummy
         lut = {}
         
+        
+        # first pass, do two things: 1) link the next node for each new node in the newly copied linked list 
+        #                            2) Establish a link between original non-empty node to the newly created node (copied node), such that any NON-EMPTY original node has a mapped new Node
         while curr.next:
+            #1) link the next node for each new node in the newly copied linked list 
             currNew.next = Node(curr.next.val)
+            # advance both curr and currNew ptr
             curr, currNew = curr.next, currNew.next
             
+            #2) Establish a link between original non-empty node to the newly created node (copied node), such that any NON-EMPTY original node has a mapped new Node
             lut[curr] = currNew
             
         
+        #reset both curr and currNew ptr to the heads
         curr, currNew = dummy.next, newDummy.next
+        
+        # second pass, do one thing: 1) connect the random ptr for each new node in the newly coplied linked list, using the lut created in the first pass above. 
+        
+        #loop through each original node
         while curr:
             
+            # original node's corresponding new node is lut[curr]. populate its random pointer, to be original node's random ptr node's mapped node(in new list), if that random ptr node is non-EMPTY. If emptry (None node), then populate with None too.
+            # This is because if original's node's random node can be None , where None node will not be in lut, since only non-EMPTY node was saved into LUT
             lut[curr].random = lut[curr.random] if curr.random else None
             curr = curr.next
             
