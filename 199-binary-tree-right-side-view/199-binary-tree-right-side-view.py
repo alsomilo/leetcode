8f13d365e0845e73e45dbf8895f58b78224c45c9
yuanw <35405452+alsomilo@users.noreply.github.com>
2022-10-01 15:41:11 -0700

    Time: 47 ms (69.34%), Space: 13.8 MB (71.14%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -4,57 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         
-        '''
-        if not root:
-            return []
-        
-        fifo = deque()
-        fifo.append(root)
-        res = []
-        
-        while fifo:
-            
-            currLen = len(fifo)
-            
-            for i in range(currLen):
-                currNode = fifo.popleft()
-                
-                
-                if i == currLen - 1:
-                    res.append(currNode.val)
-        
-                if currNode.left:
-                    fifo.append(currNode.left)
-                    
-                if currNode.right:   
-                    fifo.append(currNode.right)
-                    
-        
-        return res
-        
-        '''
-        
-        '''
-        def dfs(root):
-            if not root:
-                return []
-            
-            left = dfs(root.left)
-            right = dfs(root.right)
-            
-            return [root.val] + right + left[len(right):]
-        
-        
-        return dfs(root)
-        '''
-        
         view = []
         
-        def dfs(root,depth):
+        def dfs(root, depth):
             if not root:
                 return
             
@@ -63,7 +18,7 @@ class Solution:
                 
             dfs(root.right, depth+1)
             dfs(root.left, depth+1)
+            
         
         dfs(root, 0)
-        
         return view
