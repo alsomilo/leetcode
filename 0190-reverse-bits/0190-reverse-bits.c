2607b0937a722dfbb3e05e6ee6800edcebcce299
yuanw <35405452+alsomilo@users.noreply.github.com>
2022-11-02 23:24:00 -0700

    Time: 3 ms (53.36%), Space: 5.4 MB (70.27%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.c
+++ 0190-reverse-bits/0190-reverse-bits.c
@@ -1,5 +1,6 @@
 uint32_t reverseBits(uint32_t n) {
     
+    /*
     uint32_t res = 0;
     
     for(int i = 0; i < 32; i++)
@@ -10,4 +11,15 @@ uint32_t reverseBits(uint32_t n) {
     }
         
     return res;
+    */
+    
+    
+    n = (n << 16) | (n >> 16);
+    n = ((n & 0x00FF00FF) << 8) | ((n & 0xFF00FF00) >> 8);
+    n = ((n & 0x0F0F0F0F) << 4) | ((n & 0xF0F0F0F0) >> 4);
+    n = ((n & 0x33333333) << 2) | ((n & 0xCCCCCCCC) >> 2);
+    n = ((n & 0x55555555) << 1) | ((n & 0xAAAAAAAA) >> 1);
+    
+    return n;
+    
 }
