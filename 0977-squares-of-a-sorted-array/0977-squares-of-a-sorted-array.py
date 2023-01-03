567eb9694cfd282c8d5e895bd477c6f21dcd775f
yuanw <35405452+alsomilo@users.noreply.github.com>
2023-01-02 22:53:59 -0800

    Time: 464 ms (47.86%), Space: 16.4 MB (16.94%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,15 +1,29 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        i,j = 0, len(nums)-1
+        
+        #two pointers
+        
+        #method 1: use two pointers: since the original input array is already sorted, the min value is at most left, and max value at most right. 
+        # contine the while loop as long as i <= j, pls note we need the <= here , not <. because when i ==j, the value still need to be squred and saved into target array
+        # for each iteration, check which abs value is bigger, and save the bigger one's squred value from right to left (append value to the left of the res array) and move the i or j accordingly 
+        
+        '''
+        i,j = 0,len(nums)-1
         res = []
         
         while i <= j:
-            
             if abs(nums[i]) < abs(nums[j]):
-                res = [nums[j] **2] + res
+                res = [nums[j] ** 2] + res
                 j-=1
             else:
-                res = [nums[i] **2] + res
+                res = [nums[i] ** 2] + res
                 i+=1
-                
+        
         return res
+        '''
+        #'''
+        #method 2:  directly generated new squared array first, and then sort
+        res = [num**2 for num in nums]
+        res.sort()
+        return res
+        #'''
